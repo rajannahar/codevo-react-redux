1dd6fe5f1efe52291348c5302ccc887288c2a92f
rajannahar <R_N101@msn.com>
2020-02-02 16:30:16 +0000

    added redux logger and redux devtools

--- react-redux-demo/src/redux/store.js
+++ react-redux-demo/src/redux/store.js
@@ -1,6 +1,9 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './rootReducer'
+import logger from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer)
+const devTools = composeWithDevTools(applyMiddleware(logger))
+const store = createStore(rootReducer, devTools)
 
 export default store
